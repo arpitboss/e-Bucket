2f5a556e0cb82d7e78900a1160c63cda00b4e9d1
Arpit <arpit123boss@gmail.com>
2024-01-11 19:23:25 +0530

    test commit

--- e-Bucket/lib/main.dart
+++ e-Bucket/lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
-import 'app.dart';
 import 'data/repositories/authentication/authentication_repository.dart';
 import 'firebase_options.dart';
 
@@ -22,5 +21,5 @@ Future<void> main() async {
     (FirebaseApp value) => Get.put(AuthenticationRepository()),
   );
 
-  runApp(const App());
+  // runApp(const App());
 }
