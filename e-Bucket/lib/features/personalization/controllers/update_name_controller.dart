b4c5263867f9b5590c6cf471509c526a945c8022
Arpit <arpit123boss@gmail.com>
2024-02-15 04:44:13 +0530

    added CRUD operations

---
+++ e-Bucket/lib/features/personalization/controllers/update_name_controller.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:t_store/data/repositories/user/user_repository.dart';
+import 'package:t_store/features/personalization/controllers/user_controller.dart';
+
+import '../../../utils/constants/image_strings.dart';
+import '../../../utils/helpers/network_manager.dart';
+import '../../../utils/popups/full_screen_loader.dart';
+import '../../../utils/popups/loaders.dart';
+import '../screens/profile/profile.dart';
+
+class UpdateNameController extends GetxController {
+  final firstName = TextEditingController();
+  final lastName = TextEditingController();
+  final userController = UserController.instance;
+  final userRepository = Get.put(UserRepository());
+
+  GlobalKey<FormState> updateUserNameFormKey = GlobalKey<FormState>();
+
+  @override
+  void onInit() {
+    initializeNames();
+    super.onInit();
+  }
+
+  /// Fetch user record
+  Future<void> initializeNames() async {
+    firstName.text = userController.user.value.firstName;
+    lastName.text = userController.user.value.lastName;
+  }
+
+  Future<void> updateUserName() async {
+    try {
+      // Start Loading
+      FullScreenLoader.openLoadingDialog(
+          'We are updating your information...', TImages.docerAnimation);
+
+      // Check Internet Connectivity
+      final isConnected = await NetworkManager.instance.isConnected();
+      if (!isConnected) {
+        FullScreenLoader.stopLoading();
+        return;
+      }
+
+      // Form Validation
+      if (!updateUserNameFormKey.currentState!.validate()) {
+        FullScreenLoader.stopLoading();
+        return;
+      }
+
+      // Update user's first & last name in the Firebase Firestore
+      Map<String, dynamic> name = {
+        'FirstName': firstName.text.trim(),
+        'LastName': lastName.text.trim()
+      };
+      await userRepository.updateSingleField(name);
+
+      // Update the Rx User value
+      userController.user.value.firstName = firstName.text.trim();
+      userController.user.value.lastName = lastName.text.trim();
+
+      // Remove Loader
+      FullScreenLoader.stopLoading();
+
+      // Show Success Message
+      Loaders.successSnackBar(
+          title: 'Congratulations', message: 'Your Name has been updated.');
+
+      // Move to previous screen.
+      Get.off(() => const ProfileScreen());
+    } catch (e) {
+      FullScreenLoader.stopLoading();
+      Loaders.errorSnackBar(title: 'Oh Snap!', message: e.toString());
+    }
+  }
+}
